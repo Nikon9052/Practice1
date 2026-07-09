f95c815af8cb32b8ca7034f4e0b557dc09426dd6
Nikon Donetskiy <nikon.don.48@gmail.com>
2026-07-09 22:46:41 +0300

    Сохранение файла

--- task1.py
+++ task1.py
@@ -2,15 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
+# Параметры
 A = 418.9829
 x_min = -500
 x_max = 500
-num_points = 2000  
-output_dir = "results"
+num_points = 2000
+output_dir = r"C:\Study\Practice\task_1\results"
 output_file = os.path.join(output_dir, "result.txt")
 
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
-    print(f"Директория '{output_dir}' создана.")
-else:
-    print(f"Директория '{output_dir}' уже существует.")
+os.makedirs(output_dir, exist_ok=True)
+print(f"✅ Папка '{output_dir}' создана!")
+
+x = np.linspace(x_min, x_max, num_points)
+y = A - x * np.sin(np.sqrt(np.abs(x)))
+print(f"✅ Расчет выполнен для {num_points} точек")
+
+with open(output_file, "w", encoding="utf-8") as file:
+    file.write(f"# Результаты расчета функции f(x) = A - x * sin(sqrt(|x|))\n")
+    file.write(f"# A = {A}, x in [{x_min}; {x_max}], points = {num_points}\n")
+    for xi, yi in zip(x, y):
+        file.write(f"{xi:.6f}    {yi:.6f}\n")
+print(f"✅ Результаты сохранены в файл: {output_file}")
