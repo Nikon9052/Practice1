10e0af6bc63ccb56196bc56f5f3506511e6ff603
Nikon Donetskiy <nikon.don.48@gmail.com>
2026-07-16 01:36:23 +0300

    Исправление ошибок

--- task_2/task2.py
+++ task_2/task2.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-def f(x1, x2): return 0.5 + ((x1**2 - x2**2)**2 - 0.5) / (1 + 0.001 * (x1**2 + x2**2))**2
+def f(x1, x2): 
+    return 0.5 + (np.sin(x1**2 - x2**2)**2 - 0.5) / (1 + 0.001 * (x1**2 + x2**2))**2
 
 x1_range = [-2.0, 2.0]
 x2_range = [-2.0, 2.0]
